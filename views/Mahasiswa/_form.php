167294eba4f4b5a24ae264a3ed3f93d3a55b798f
sweeetboyz <80476507+sweeetboyz@users.noreply.github.com>
2021-06-12 03:05:55 +0700

    Add files via upload

---
+++ views/Mahasiswa/_form.php
@@ -0,0 +1,50 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper;
+use kartik\date\DatePicker;
+use app\models\Prodi;
+
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Mahasiswa */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="mahasiswa-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'nim')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'nama')->textInput(['maxlength' => true]) ?>
+
+
+    <?php $model->isNewRecord==1? $model->jekel='L':$model->jekel; ?>
+
+    <?= $form->field($model, 'jekel')->radioList(array('L'=>'Laki-laki','P'=>'Perempuan'))->label('Jenis Kelamin') ?>
+
+    
+
+    <?= $form->field($model, 'tgl')->widget(DatePicker::classname(), [
+        'options' => ['placeholder' => 'Pilih Tanggal ...'],
+        'pluginOptions' => [
+        'autoclose'=>true,
+        'format' => 'dd-M-yyyy'
+    ]
+]);?>
+
+  <?= $form->field($model, 'id_prodi')->dropDownList(ArrayHelper::map(Prodi::find()->all(),'id','prodi'),['prompt' => 'Pilih'])->label('Prodi') ?>
+
+    <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'alamat')->textarea(array('rows'=>4)) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
---
+++ views/prodi/_form.php
@@ -0,0 +1,25 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Prodi */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="prodi-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'prodi')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'keterangan')->textInput(['maxlength' => true]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
